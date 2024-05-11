bd3bf31b4684231448b7cb5cb6a45f93873812e9
Bhavesh Ranjan <bhaveshranjanswork@gmail.com>
2024-05-11 18:54:44 +0530

    Println vs Printf

---
+++ print/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	age := 25
+	name := "Alice"
+	height := 5.5876
+
+	fmt.Println("age: ", age, "height ", height, "name ", name)
+
+	fmt.Printf("age is %d\n", age)
+	fmt.Printf("Type of height is %T\n", height)
+}
