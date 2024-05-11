e8d6bb40b0da8a2a4a8676b82fe2452c90be2511
Bhavesh Ranjan <bhaveshranjanswork@gmail.com>
2024-05-11 18:49:07 +0530

    .....

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"practice/myutil"
+)
+
+func main() {
+
+	fmt.Println("Hello Bhavesh")
+	myutil.Printmessage("printing My util")
+
+	var name string = "Bhavesh"
+	fmt.Println(name)
+
+	var money = 67
+	fmt.Println(money)
+
+	var message string = "Keep Trying"
+	fmt.Println("This is my Message:   ", message)
+}
