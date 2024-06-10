9f72c893c88422c8908c887759bfaca1512d9233
Bhavesh Ranjan <bhaveshranjanswork@gmail.com>
2024-06-10 08:14:37 +0530

    CRUD  Operation and POST method

--- crud/main.go
+++ crud/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
 type Todo struct {
@@ -13,8 +15,7 @@ type Todo struct {
 	Completed bool   `json:"completed"`
 }
 
-func main() {
-	fmt.Println("Learning CRUD...")
+func performGetRequest() {
 	res, err := http.Get("https://jsonplaceholder.typicode.com/todos/1")
 	if err != nil {
 		fmt.Println("Error getting : ", err)
@@ -47,6 +48,44 @@ func main() {
 		return
 	}
 	fmt.Println("Todo: ", todo)
+}
+
+func performPostRequest() {
+	todo := Todo{
+		UserID:    23,
+		Title:     "Bhavesh Ranjan",
+		Completed: true,
+	}
+	//Convert the Todo struct to JSON
+	jsonData, err := json.Marshal(todo)
+	if err != nil {
+		fmt.Println("Error in marshalling: ", err)
+		return
+	}
+
+	//convert json data to string
+	jsonString := string(jsonData)
+
+	//convert string data to reader
+	jsonReader := strings.NewReader(jsonString)
+
+	myURL := "https://jsonplaceholder.typicode.com/todos"
+	res, err := http.Post(myURL, "application/json", jsonReader)
+	if err != nil {
+		fmt.Println("Error sending request : ", err)
+		return
+	}
+
+	defer res.Body.Close()
+
+	data, _ := ioutil.ReadAll(res.Body)
+	fmt.Println("Response : ", string(data))
+
+}
+func main() {
+	fmt.Println("Learning CRUD...")
+	performPostRequest()
+	performGetRequest()
 }
 
 //Output:=
