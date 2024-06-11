123f5147d788a9da4d87d4db6690a440380c111f
Bhavesh Ranjan <bhaveshranjanswork@gmail.com>
2024-06-11 08:19:15 +0530

    CRUD Operation || POST, UPDATE, DELETE Method

--- crud/main.go
+++ crud/main.go
@@ -82,13 +82,99 @@ func performPostRequest() {
 	fmt.Println("Response : ", string(data))
 
 }
+
+func performUpdateRequest() {
+	todo := Todo{
+		UserID:    23789,
+		Title:     "Bhavesh Ranjan Updating",
+		Completed: false,
+	}
+
+	//Convert the Todo struct to JSON
+	jsonData, err := json.Marshal(todo)
+	if err != nil {
+		fmt.Println("Error in marshalling: ", err)
+		return
+	}
+
+	//convert json data to string
+	jsonString := string(jsonData)
+
+	//convert string data to reader
+	jsonReader := strings.NewReader(jsonString)
+
+	const myurl = "https://jsonplaceholder.typicode.com/todos/1"
+
+	//Create PUT Request
+	req, err := http.NewRequest(http.MethodPut, myurl, jsonReader)
+	if err != nil {
+		fmt.Println("Error creating PUT Request : ", err)
+		return
+	}
+	req.Header.Set("Content-type", "application/json")
+
+	//Send the request
+	client := http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error sending request : ", err)
+		return
+	}
+	defer res.Body.Close()
+
+	data, _ := ioutil.ReadAll(res.Body)
+	fmt.Println("Response : ", string(data))
+	fmt.Println("Response status : ", res.Status)
+
+}
+
+func performDeleteRequest() {
+	const myurl = "https://jsonplaceholder.typicode.com/todos/1"
+
+	//Create Delete Request
+	req, err := http.NewRequest(http.MethodDelete, myurl, nil)
+	if err != nil {
+		fmt.Println("Error creating Delete Request : ", err)
+		return
+	}
+	//Send the request
+	client := http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error sending request : ", err)
+		return
+	}
+	defer res.Body.Close()
+
+	fmt.Println("Response status : ", res.Status)
+
+}
+
 func main() {
 	fmt.Println("Learning CRUD...")
-	performPostRequest()
-	performGetRequest()
+	// performPostRequest()
+	// performGetRequest()
+	//performUpdateRequest()
+	performDeleteRequest()
 }
 
 //Output:=
 
 // Learning CRUD...
 // Todo:  {1 1 delectus aut autem false}
+
+// Output:= for Update function
+
+// Learning CRUD...
+// Response :  {
+//   "userId": 23789,
+//   "id": 1,
+//   "title": "Bhavesh Ranjan Updating",
+//   "completed": false
+// }
+// Response status :  200 OK
+
+//Output := for Delete Function
+
+// Learning CRUD...
+// Response status :  200 OK
