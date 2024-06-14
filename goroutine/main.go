91ca16dcdcc7e9886563a0817de6cbd1aa25be57
Bhavesh Ranjan <bhaveshranjanswork@gmail.com>
2024-06-14 07:48:00 +0530

    GO Routine

---
+++ goroutine/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func sayHello() {
+	fmt.Println("Hello World !")
+	time.Sleep(2000 * time.Millisecond) // Simulating some work
+	fmt.Println("Say hello function ended")
+}
+
+func sayHi() {
+	fmt.Println("Hi Bhavesh :)")
+}
+
+func main() {
+	fmt.Println("learning goroutines")
+	go sayHello()
+	go sayHi()
+
+	//wait for a moment to allow the go routine to finish
+
+	time.Sleep(3000 * time.Millisecond)
+}
